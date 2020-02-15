918b3a9e87876f75e5fc5861717ee13e9b654379
Matthew Flood <hermannwest@gmail.com>
2020-02-15 09:09:51 -0700

    Create setup.sh script to build the initial blank project

---
+++ setup.sh
@@ -0,0 +1,22 @@
+
+# This is the script I kiicked off to create the empty app
+# and install the npm packages required
+
+# Install npm modules
+function install_npm_modules() {
+    npm install prop-types
+    npm install --save escape-string-regexp sort-by
+
+    npm install --save react-router-dom
+    npm install --save form-serialize
+}
+install_npm_modules
+
+# Build initial blank react app
+function bootstrap_myreads_app() {
+    create-react-app myreads
+}
+# bootstrap_myreads_app
+
+
+
