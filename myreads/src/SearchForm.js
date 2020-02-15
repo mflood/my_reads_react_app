2552b0006d0eb986774c8bd5995f02985c590c78
Matthew Flood <hermannwest@gmail.com>
2020-02-15 16:01:09 -0700

    Copy search form HTML from starter project

--- myreads/src/SearchForm.js
+++ myreads/src/SearchForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 
 class SearchForm extends Component {
@@ -13,8 +14,40 @@ class SearchForm extends Component {
       })  
   }
 
+  handleSearchTextChanged = () => {
+
+
+   
+
+  }
+
   render() {
-       return (<div>A SearchForm</div>)
+    return (
+<div className="search-books">
+            <div className="search-books-bar">
+              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+              <div className="search-books-input-wrapper">
+                {/* 
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author"/>
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+    )
+
+
+
+
   }
 
 }
